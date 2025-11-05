08d6afde5527e6a7f33208af226aa8c41111f747
Kristofer <kris@youngers.org>
2025-11-05 10:22:28 -0500

    adds step zero

--- python/app.py
+++ python/app.py
@@ -2,8 +2,17 @@ class Bin:
     pass
 
 class Dice:
-    pass
+    def roll(self):
+        print("roll!")
+
+    def display(self):
+        print("rolled! thanks!")
+
 
 class Simulation:
     pass
 
+if __name__ == "__main__":
+    die = Dice()
+    die.roll()
+    die.display()
